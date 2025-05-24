07da2fc392a37b1fd6b4b03822760be90cfc5468
Dmitry Lyapin <lyapindm@yandex.ru>
2025-05-24 08:57:22 +0300

    Add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 import packageJson from "./package.json" with { type: "json" }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    // Allows using the compilerOptions.paths property in tsconfig.json.
+    // https://www.npmjs.com/package/vite-tsconfig-paths
+    tsconfigPaths(),
+],
 
   server: {
     open: true,
