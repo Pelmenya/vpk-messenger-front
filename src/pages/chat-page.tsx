92a51a38b55d0df71f06a2003d64fa0c440bf106
Dmitry Lyapin <lyapindm@yandex.ru>
2025-05-25 11:37:49 +0300

    Add Outlet for chat view

--- src/pages/chat-page.tsx
+++ src/pages/chat-page.tsx
@@ -1,15 +1,16 @@
 import { Header } from "@/widgets/header/header"
 import { Layout } from "../shared/ui/layout/layout"
 import { ChatSidebar } from "@/entities/chat/ui/chat-sidebar/chat-sidebar"
+import { Outlet } from "react-router-dom"
 
 export const ChatPage = () => {
   return (
     <Layout isViewHeader={false}>
       <div className="w-full h-full max-w-7xl mx-auto flex bg-base-100">
-        <ChatSidebar
-          header={<Header className="flex justify-end" />}
-        />
-        <aside className="flex-1 h-full"></aside>
+        <ChatSidebar header={<Header className="flex justify-end" />} />
+        <aside className="flex-1 h-full">
+          <Outlet />
+        </aside>
       </div>
     </Layout>
   )
