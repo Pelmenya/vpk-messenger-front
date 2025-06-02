77a993188a088deefa24a6db1d44ee1424b4c03c
Dmitry Lyapin <lyapindm@yandex.ru>
2025-06-02 10:50:12 +0300

    Add location for different header view

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -4,12 +4,15 @@ import { getUser } from "@/entities/user/model/user-selectors"
 import { ProfileMenuDropdown } from "@/features/auth/ui/profile-menu-dropdown/profile-menu-dropdown"
 import { ThemeToggle } from "@/widgets/theme-toggle/theme-toggle"
 import cn from "classnames"
+import { useLocation } from "react-router-dom"
 
 export const Header: FC<{ children?: ReactNode; className?: string }> = ({
   className,
   children,
 }) => {
+  const location = useLocation();
   const user = useAppSelector(getUser)
+  const isPofile  = location.pathname.split('/').includes('profile');
 
   return (
     <header
@@ -21,7 +24,7 @@ export const Header: FC<{ children?: ReactNode; className?: string }> = ({
     >
       <div className="flex items-center gap-2">
         <ThemeToggle />
-        {user && <ProfileMenuDropdown user={user} />}
+        {user && !isPofile && <ProfileMenuDropdown user={user} />}
         {children}
       </div>
     </header>
