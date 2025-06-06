3c1a3944c5238490aece35a1b03ab0cdcfe2475f
Dmitry Lyapin <lyapindm@yandex.ru>
2025-06-06 09:00:48 +0300

    Add tostify edit profile and add chat

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,8 @@
-import { AuthProvider } from "./auth-provider";
-import { AuthRouter } from "./auth-router";
+import { AuthProvider } from "./auth-provider"
+import { AuthRouter } from "./auth-router"
 
 export const App = () => (
   <AuthProvider>
-    {(isAuthLoading: boolean) => <AuthRouter isAuthLoading={isAuthLoading} />}
+      {(isAuthLoading: boolean) => <AuthRouter isAuthLoading={isAuthLoading} />}
   </AuthProvider>
-);
+)
